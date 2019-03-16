53ad3ccf317f3a4d35e2ddb1bd44bc135180e99b
vbrazas <appleitunesstore11@gmail.com>
2019-03-16 19:40:58 +0200

    updated docker_init script again

--- docker_init.sh
+++ docker_init.sh
@@ -11,11 +11,16 @@
 # to link ~/.docker file to some big tmp storage either ~/goinfre or /tmp
 TMP_DIR="$HOME/goinfre/"
 
-[[ -d $HOME/.docker ]] && cp -R $HOME/.docker ${TMP_DIR}${USER}_docker/
-rm -rf $HOME/.docker
+if [[ -d $HOME/.docker ]]; then
+    cp -R $HOME/.docker ${TMP_DIR}${USER}_docker/
+else
+    mkdir ${TMP_DIR}${USER}_docker/
+fi
 
 docker-machine create --virtualbox-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/v18.09.1/boot2docker.iso -d virtualbox default
 
 docker-machine stop default
+rm -rf $HOME/.docker
 ln -s ${TMP_DIR}${USER}_docker/ $HOME/.docker
 docker-machine start default
+eval $(docker-machine env default)
